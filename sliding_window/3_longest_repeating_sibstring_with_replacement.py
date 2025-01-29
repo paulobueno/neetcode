9725f070634574cdb4f6e306ab7ef4876b963a8d
Paulo Bueno <paulob.bruno@gmail.com>
2025-01-29 18:56:56 -0300

    adding optimized resolution

--- sliding_window/3_longest_repeating_sibstring_with_replacement.py
+++ sliding_window/3_longest_repeating_sibstring_with_replacement.py
@@ -1,29 +1,25 @@
 # URL https://neetcode.io/problems/longest-repeating-substring-with-replacement
-# Time Complexity: O(n*m)
+# Time Complexity: O(n)
 # Space Complexity: O(m)
-# NOTE: There is an optimal solution using sliding window. check the website.
 
 from typing import List
 
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        res = 0
-        char_set = set(s)
-
-        for char in char_set:
-            l = count = 0
-            for r in range(len(s)):
-                if s[r] == char:
-                    count += 1
-                while (r-l+1)-count > k:
-                    if s[l] == char:
-                        count -= 1
-                    l += 1
-                res = max(res,(r-l+1))
-
-        return res
+        count_map = {}
+        max_substring = max_frequency = l = 0
+        for r, letter in enumerate(s):
+            count_map[letter] = count_map.get(letter, 0) + 1
+            max_frequency = max(count_map[letter], max_frequency)
+            while (r - l + 1) - max_frequency > k:
+                count_map[s[l]] -= 1
+                l += 1
+            max_substring = max((r - l + 1), max_substring)
+        return max_substring
 
 
 if __name__ == '__main__':
     print(Solution().characterReplacement("AAABABB",1)==5)
     print(Solution().characterReplacement("XYYX",2)==4)
+    print(Solution().characterReplacement("AAAABBBBBAAAA",2)==7)
+
